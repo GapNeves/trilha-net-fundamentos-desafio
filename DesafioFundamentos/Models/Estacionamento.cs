0604d7af8bd25a7fb40ee93932ba756c5ba2e83a
GapNeves <gabriel.neves84@hotmail.com>
2024-03-27 19:27:55 -0300

    feat: Adding new feature AdicionarVeiculo

--- DesafioFundamentos/Models/Estacionamento.cs
+++ DesafioFundamentos/Models/Estacionamento.cs
@@ -12,11 +12,14 @@ namespace DesafioFundamentos.Models
             this.precoPorHora = precoPorHora;
         }
 
+        // IMPLEMENTADO!!
         public void AdicionarVeiculo()
         {
             // TODO: Pedir para o usuário digitar uma placa (ReadLine) e adicionar na lista "veiculos"
             // *IMPLEMENTE AQUI*
             Console.WriteLine("Digite a placa do veículo para estacionar:");
+            string newCar = Console.ReadLine();
+            veiculos.Add(newCar);
         }
 
         public void RemoverVeiculo()
